9411bde94fd00e746db5c719c8e93a1037e13e88
rojalinb <rojalinb@Newt-1607-11.newtglobal.chn.local>
2016-05-18 15:04:51 +0530

    test

---
+++ myMavenProject1/src/main/java/myMavenProject1/Demo.java
@@ -0,0 +1,12 @@
+package myMavenProject1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello ducker");
+		// TODO Auto-generated method stub
+
+	}
+
+}
